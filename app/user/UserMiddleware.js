ffad5f2684bf9edf493a9e4b61f4694607426f49
avleen <mehak.bakshi201@gmail.com>
2023-03-28 22:06:50 +0530

    Building authentication middleware

---
+++ app/user/UserMiddleware.js
@@ -0,0 +1,33 @@
+
+const userSchema = require("./UserSchema");
+
+const authenticateUserMiddleware = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    res.status(400).json({
+      status: false,
+      message: "Token not found",
+    });
+    return;
+  }
+
+  const user = await userSchema.findOne({
+    "tokens.accessToken.token": token,
+  });
+  if (!user) {
+    res.status(422).json({
+      status: false,
+      message: "Invalid Token",
+    });
+    return;
+  }
+
+
+  req.user = user;
+  next();
+};
+
+module.exports = {
+  authenticateUserMiddleware,
+};
