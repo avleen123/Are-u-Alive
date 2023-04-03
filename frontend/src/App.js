aac360384f68af657ade464ff31a882b4268197d
avleen <mehak.bakshi201@gmail.com>
2023-04-03 23:06:30 +0530

    Api integration of login and signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import './App.css';
+import React, {  useState } from "react";
 
-function App() {
-  return (
-    <div className="app">
-     
-    </div>
+import Auth from "./components/Auth/Auth";
+
+import "./App.css";
+function App(){
+ 
+  return(
+<div className="app"><Auth/></div>
   );
 }
-
 export default App;
